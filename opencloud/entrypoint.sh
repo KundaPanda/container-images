4be9881c51a2c32ec4232cf98b73af4fc89b0ec0
Vojtěch Dohnal <dohnalv@proton.me>
2025-12-03 13:24:52 +0100

    fix(opencloud): entrypoint config handling

--- opencloud/entrypoint.sh
+++ opencloud/entrypoint.sh
@@ -2,19 +2,23 @@
 set -e
 
 echo "Checking presence of opencloud.yaml config"
-if [ ! -f /etc/opencloud.yaml ]; then
+if [ ! -f /var/lib/opencloud/opencloud.yaml ]; then
   echo "Creating a new opencloud config"
   /usr/bin/opencloud init --insecure=false 1> /dev/null;
+  /bin/cp -a /etc/opencloud/opencloud.yaml /var/lib/opencloud/;
 else
-  echo "Config exists, continuing";
+  echo "Config exists, linking it";
+  /bin/ln -sf /var/lib/opencloud/opencloud.yaml /etc/opencloud/opencloud.yaml;
 fi
 
 if [ -d /etc/opencloud-configs ] && [ -z "$(find /etc/opencloud-configs/ -type d -empty)" ]; then
   echo "Copying opencloud config files";
-  /bin/cp -aL /etc/opencloud-configs/* /etc/opencloud/;
+  /bin/cp -L /etc/opencloud-configs/*.yaml /etc/opencloud/;
 else
   echo "Config directory /etc/opencloud-configs doesn't exist. Skipping config copy."
 fi
+
+/bin/cp -a /var/lib
 
 echo "Starting Opencloud"
 
